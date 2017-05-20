ece589021a9b17f1a3a7a64c8d5256b8ca6c6df5
Kyle Kempt <kyle.kempt@gmail.com>
2017-05-19 21:56:45 -0500

    rest of folder

---
+++ client_login_action.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<body>
+
+
+
+<?php
+
+//echo "Username: ";
+//echo $_POST["username"];
+//echo "<br>";
+//echo "Password: ", $_POST["password"];
+
+header('Location: client_home.html');
+?>
+</body>
+</html>
